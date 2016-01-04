3c5d4c380c14d66d80548fc8937293c73a897147
Monika <moblahova@centrum.cz>
2016-01-04 23:16:40 +0100

    Refactored & Cleaned

--- imageProcessor.py
+++ imageProcessor.py
@@ -1,53 +1,52 @@
 __author__ = 'Monika'
 
-from pylab import imshow, show, get_cmap
-import math
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageEnhance
+import random
 
+# returns image with reduced color count
+def prepare_original_image(file_name):
 
-image = Image.new('RGB', (512,512), "white")
-pixelsData = image.load()
+    new_image = Image.open(file_name)
 
-new_image = Image.open('ortofoto_2.jpg')
-new_image = new_image.filter(ImageFilter.MedianFilter(size=5))
+    ImageEnhance.Contrast(new_image)
+    ImageEnhance.Sharpness(new_image)
 
-new_image.show()
+    new_image = new_image.convert('P', palette=Image.ADAPTIVE, colors=16)
+    return new_image.convert('RGB', palette=Image.ADAPTIVE)
 
-new_img_pixels = new_image.load()
+def process_image(prepared_image):
+    new_img_pixels = prepared_image.load()
 
-for y in range(new_image.size[0]):
-    for x in range(new_image.size[1]):
-        r, g, b = new_image.getpixel((y,x))
+    for y in range(prepared_image.size[0]):
+        for x in range(prepared_image.size[1]):
+            current_pixel = prepared_image.getpixel((y, x))
+            new_img_pixels[y, x] = change_pixel_rgb(current_pixel)
 
-        #print r, g, b
+    return prepared_image
 
-        pixel_avg_value = (r + g + b) / 3
+def change_pixel_rgb((r, g, b)):
+    pixel_avg_value = (r + g + b) / 3
 
-        #print pixel_avg_value
+    if abs(pixel_avg_value - r) < abs(pixel_avg_value - g) and abs(pixel_avg_value - r) < abs(pixel_avg_value - b):
+        return create_pixel(255, 0, 0)
+    elif abs(pixel_avg_value - g) < abs(pixel_avg_value - r) and abs(pixel_avg_value - g) < abs(pixel_avg_value - b):
+        return create_pixel(0, 255, 0)
+    elif abs(pixel_avg_value - b) < abs(pixel_avg_value - g) and abs(pixel_avg_value - b) < abs(pixel_avg_value - r):
+        return create_pixel(0, 0, 255)
+    else:
+        return create_pixel(100, 100, 100)
 
-        if abs(pixel_avg_value - r) < abs(pixel_avg_value - g) and abs(pixel_avg_value -r) < abs(pixel_avg_value - b):
-            r = 255
-            g = 0
-            b = 0
-        elif abs(pixel_avg_value - g) < abs(pixel_avg_value - r) and abs(pixel_avg_value -g) < abs(pixel_avg_value - b):
-            r = 0
-            g = 255
-            b = 0
-        elif abs(pixel_avg_value - b) < abs(pixel_avg_value - g) and abs(pixel_avg_value -b) < abs(pixel_avg_value - r):
-            r = 0
-            g = 0
-            b = 255
-        else:
-            r = 100
-            g = 100
-            b = 100
+# returns rgb tuple, where all values are cast to int type
+def create_pixel(r, g, b):
+    return (int(r), int(g), int(b))
 
-        new_img_pixels[y, x] = (int(r), int(g), int(b))
+def show_image(image):
+    image.show()
 
-new_image.show()
+def export_image(image):
+    image.save("output_image" + str(random.randint(1,10000)) + ".jpg")
 
-
-
-
-
+image = process_image(prepare_original_image("ortofoto_2.jpg"))
+show_image(image)
+export_image(image)
 
