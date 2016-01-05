cbc0464bbac44a4d730d78a32539bc432c5d03ac
Monika <moblahova@centrum.cz>
2016-01-05 12:53:58 +0100

    Refactored & Cleaned

--- dataMiner.py
+++ dataMiner.py
@@ -1,108 +1,99 @@
 import csv
 import pygal
 from datetime import datetime
-import matplotlib.pyplot as plt
 import numpy as np
+import urllib
+import random
+
+def download_data(url):
+    file = urllib.urlretrieve (url, "test.csv")
+    return file
 
 def get_csv_from_file(file):
     file_c = open(file)
     csv_file = csv.reader(file_c)
     return csv_file
 
+get_csv_from_file(download_data("http://www.mapakriminality.cz/data/csv?areacode=001214&crimetype=101-903&timefrom=120&timeto=130"))
+
+def remove_table_headers (csv_file_row):
+    return csv_file_row.remove(csv_file_row[0])
+
+def cast_int(csv_file_row):
+    return [int(x) for x in csv_file_row]
+
+def create_numpy_array(csv_file_row):
+    return np.array(csv_file_row)
+
 def prepare_data(csv_file):
-    crimes = []
+    crimes_count = []
     solved = []
     recidivism = []
     all = []
 
     for row in csv_file:
         all.append(row[0])
-        crimes.append(row[1])
+        crimes_count.append(row[1])
         solved.append(row[2])
         recidivism.append(row[6])
 
-    crimes.remove(crimes[0])
-    solved.remove(solved[0])
-    recidivism.remove(recidivism[0])
-    all.remove(all[0])
+    remove_table_headers(crimes_count)
+    remove_table_headers(solved)
+    remove_table_headers(recidivism)
+    remove_table_headers(all)
 
-    crimes = [int(x) for x in crimes]
-    solved = [int(x) for x in solved]
-    recidivism = [int(x) for x in recidivism]
-    all = [float(x) for x in all]
+    crimes_count = cast_int(crimes_count)
+    solved = cast_int(solved)
+    recidivism = cast_int(recidivism)
+    all = cast_int(all)
 
-    crimes = np.array(crimes)
-    crimes[crimes < 0] = 0
-    solved = np.array(solved)
+    crimes_count = create_numpy_array(crimes_count)
+    crimes_count[crimes_count < 0] = 0
+    solved = create_numpy_array(solved)
     solved[solved < 0] = 0
-    recidivism = np.array(recidivism)
+    recidivism = create_numpy_array(recidivism)
     recidivism[recidivism < 0] = 0
-    all = np.array(all)
+    all = create_numpy_array(all)
     all[all < 0] = 0
 
-    crimes = crimes [0:29]
+    crimes_count = crimes_count [0:29]
     solved = solved [0:29]
     recidivism = recidivism [0:29]
     all = all [0:29]
 
 
-    out = [crimes, solved, recidivism, all]
+    final_data = [crimes_count, solved, recidivism, all]
 
-    return out
+    return final_data
 
-# murders = prepare_data(get_csv_from_file('vrazdy_praha_full.csv'))
-# murders_found = murders[0]
-# murders_recidive = murders[2]
-#
-# rapes = prepare_data(get_csv_from_file('znasilneni_praha_full.csv'))
-# rapes_found = rapes[0]
-# rapes_recidive = rapes[2]
-#
-# all = prepare_data(get_csv_from_file('znasilneni_praha_full.csv'))
-# rapes_found = rapes[0]
+def prepare_graph_data():
+    pass
+
+murders = prepare_data(get_csv_from_file('vrazdy_praha_full.csv'))
+murders_found = murders[0]
+murders_recidive = murders[2]
+
+rapes = prepare_data(get_csv_from_file('znasilneni_praha_full.csv'))
+rapes_found = rapes[0]
+rapes_recidive = rapes[2]
+
+all = prepare_data(get_csv_from_file('znasilneni_praha_full.csv'))
+rapes_found = rapes[0]
 
 
-# attacks = prepare_data(get_csv_from_file('fyzicky_utok_praha_full.csv'))
-# attacks_found = attacks[0]
-# attacks_recidive = attacks[2]
-# attacks_found_sum = sum(attacks_found)
-# print attacks_found_sum
-# attacks_recidive_sum = sum(attacks_recidive)
-# print attacks_recidive_sum
-# print attacks
+attacks = prepare_data(get_csv_from_file('fyzicky_utok_praha_full.csv'))
+attacks_found = attacks[0]
+attacks_recidive = attacks[2]
+attacks_found_sum = sum(attacks_found)
+print attacks_found_sum
+attacks_recidive_sum = sum(attacks_recidive)
+print attacks_recidive_sum
+print attacks
 
-cr = prepare_data(get_csv_from_file('celkova_cr.csv'))
-cr1 = cr[3]
-print (cr1)
-hk = prepare_data(get_csv_from_file('celkova_hk.csv'))
-hk1 = hk[3]
-jc = prepare_data(get_csv_from_file('celkova_jc.csv'))
-jc1 = jc[3]
-jmk = prepare_data(get_csv_from_file('celkova_jmk.csv'))
-jmk1 = jmk[3]
-karlov = prepare_data(get_csv_from_file('celkova_karlov.csv'))
-karlov1 = karlov[3]
-lib = prepare_data(get_csv_from_file('celkova_lib.csv'))
-lib1 = lib[3]
-mszk = prepare_data(get_csv_from_file('celkova_mszk.csv'))
-mszk1 = mszk[3]
-olm = prepare_data(get_csv_from_file('celkova_olm.csv'))
-olm1 = olm[3]
-pard = prepare_data(get_csv_from_file('celkova_pard.csv'))
-pard1 = pard[3]
-plz = prepare_data(get_csv_from_file('celkova_plz.csv'))
-plz1 = plz[3]
-praha = prepare_data(get_csv_from_file('celkova_praha.csv'))
-praha1 = praha[3]
-stred = prepare_data(get_csv_from_file('celkova_stredocesky.csv'))
-stred1 = stred[3]
-usti = prepare_data(get_csv_from_file('celkova_usti.csv'))
-usti1 = usti[3]
-vys = prepare_data(get_csv_from_file('celkova_vys.csv'))
-vys1 = vys[3]
-zlin = prepare_data(get_csv_from_file('celkova_zlin.csv'))
-zlin1 = zlin[3]
-
+def get_criminality_index(csv_file):
+    region_csv = prepare_data(get_csv_from_file(file))
+    region_criminality_index = region_csv[3]
+    return region_criminality_index
 
 
 date_chart = pygal.Line(x_label_rotation=45)
@@ -139,35 +130,19 @@ datetime(2015, 4, 1),
 datetime(2015, 5, 1)])
 
 # date_chart.add("Recidivist", attacks_recidive)
-date_chart.add('CR', cr1)
-date_chart.add('Hradecky', hk1)
-date_chart.add('Jihocesky', jc1)
-date_chart.add('Jihomoravsky', jmk1)
-date_chart.add('Karlovarsky', karlov1)
-date_chart.add('Liberecky', lib1)
-date_chart.add('Moravskoslezsky', mszk1)
-date_chart.add('Olomoucky', olm1)
-date_chart.add('Pardubicky', pard1)
-date_chart.add('Plzensky', plz1)
-date_chart.add('Praha', praha1)
-date_chart.add('Stredocesky', stred1)
-date_chart.add('Ustecky', usti1)
-date_chart.add('Vysocina', vys1)
-date_chart.add('Zlin', zlin1)
+def add_graph_chart(caption, data):
 
+    for i in range (caption):
+        date_chart.add(caption, data)
 
-# date_chart.add('Olomouc', gandza_found_2)
-# date_chart.add('Stredocesky', gandza_found_3)
-# date_chart.add('Jihomoravsky', gandza_found_4)
-# date_chart.add('Jihocesky', gandza_found_5)
-# date_chart.add('Attacks', attacks_found)
+def create_pie_chart(title,caption,data):
+    pie_chart = pygal.Pie()
+    pie_chart.title = title
 
-date_chart.render_to_file('example.svg')
+    for i in range (caption):
+        pie_chart.add(caption, data)
 
-# pie_chart = pygal.Pie()
-# pie_chart.title = 'Recidivists in physical attack (in %)'
-# pie_chart.add('Recidivists', 66.11)
-# pie_chart.add('New', 33.89)
-# pie_chart.render_to_file('example2.svg')
+def render_graph(file_name, graph):
+    graph.render_to_file(file_name + random.randint(1,10000))
 
 
